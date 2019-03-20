22587b04786ebc6fcb302996fab86b6fa49a6398
Maiki Okura <sheep.over@gmail.com>
2019-03-20 15:35:11 +0900

    tmp

--- main.py
+++ main.py
@@ -9,6 +9,7 @@ from torch.autograd import Variable
 from torchtext.data import Field
 from torchtext.datasets import LanguageModelingDataset
 import spacy
+import numpy as np
 spacy_en = spacy.load('en')
 
 from data_loader import DcmhDataset
@@ -17,9 +18,9 @@ from dcmh_model import CNNModel, TextModel
 
 GPU = torch.cuda.is_available()
 
-HASH_CODR_LENGTH = 100
+HASH_CODR_LENGTH = 32
 IMG_SIZE = 256
-BATCH_SIZE = 32
+BATCH_SIZE = 64
 NEPOCH = 100
 
 
@@ -85,7 +86,7 @@ def train_text(epoch, img_model, text_model, hash_matrix, sim_matrix, device, tr
 
         sim_sum = torch.sum(theta_batch*sim_matrix_batch - torch.log(1. + torch.exp(theta_batch)))
         preserve_sim = torch.sum(torch.pow(hash_matrix_batch - text_out_batch, 2))
-        preserve_balance = torch.sum(torch.pow(text_out.t().matmul(ones), 2))
+        preserve_balance = torch.sum(torch.pow(text_out.t().mm(ones), 2))
         loss = -sim_sum + gamma*preserve_sim + eta*preserve_balance
         loss /= (batch_size*ntrains)
         text_optimizer.zero_grad()
@@ -96,21 +97,21 @@ def train_text(epoch, img_model, text_model, hash_matrix, sim_matrix, device, tr
     return text_out
 
 def calc_sim_matrix(source_label, target_label, device):
-    print(target_label.shape)
-    print(source_label[0,:])
-    sim_matrix = (torch.matmul(source_label, target_label.t()) > 0).type(torch.FloatTensor).to(device)
-    print(sim_matrix)
+    sim_matrix = (torch.mm(source_label, target_label.t()) > 0).type(torch.FloatTensor).to(device)
     return sim_matrix
 
 def calc_loss(sim_matrix, hash_matrix, img_out, text_out, gamma, eta, ntrains, device):
-    ones = torch.ones(ntrains).to(device)
+    ones = torch.ones(ntrains, 1).to(device)
 
-    theta = (1./2.)*torch.matmul(img_out, text_out.t())
-    sim_sum = torch.sum(theta*sim_matrix - torch.log(1. + torch.exp(theta)))
+    theta = (1./2.)*torch.mm(img_out, text_out.t())
+    sim_sum = -torch.sum(theta*sim_matrix - torch.log(1. + torch.exp(theta)))
+    print(sim_sum)
     preserve_sim = torch.sum(torch.pow(hash_matrix - img_out, 2)) + torch.sum(torch.pow(hash_matrix - text_out, 2))
-    preserve_balance = torch.sum(torch.pow(img_out.t().matmul(ones), 2)) + torch.sum(torch.pow(text_out.t().matmul(ones), 2))
+    print(preserve_sim)
+    preserve_balance = torch.sum(torch.pow(img_out.sum(dim=0), 2)) + torch.sum(torch.pow(text_out.sum(dim=0), 2))
+    print(preserve_balance)
 
-    loss = -sim_sum + gamma*preserve_sim + eta*preserve_balance
+    loss = sim_sum + gamma*preserve_sim + eta*preserve_balance
 
     return loss
 
@@ -127,18 +128,18 @@ def train(img_model, text_model, train_data, vocab_size, device):
     train_loader_ = torch.utils.data.DataLoader(train_data, batch_size=ntrain)
     for batch_idx, (data_ids, data_idxs, imgs, tag_vecs) in enumerate(train_loader_):
         label_set = tag_vecs
-    gamma = 0
-    eta = 0
+    gamma = 1.
+    eta = 1.
 
     img_out = torch.randn([ntrain, HASH_CODR_LENGTH]).to(device)
     text_out = torch.randn([ntrain, HASH_CODR_LENGTH]).to(device)
-    ones = torch.ones(ntrain).to(device)
+    ones = torch.ones(ntrain, 1).to(device)
 
     hash_matrix = torch.sign(img_out + text_out)
     sim_matrix = calc_sim_matrix(label_set, label_set, device)
 
-    img_optimizer = optim.SGD(img_model.parameters(), lr=0.1)
-    text_optimizer = optim.SGD(text_model.parameters(), lr=0.1)
+    img_optimizer = optim.SGD(img_model.parameters(), lr=0.01)
+    text_optimizer = optim.SGD(text_model.parameters(), lr=0.01)
 
     for epoch in range(NEPOCH):
         print('epoch: ', (epoch+1))
@@ -150,31 +151,30 @@ def train(img_model, text_model, train_data, vocab_size, device):
         for batch_idx, (data_ids, data_idxs, imgs, tag_vecs) in enumerate(train_loader):
             batch_size = len(data_ids)
             imgs, tag_vecs = imgs.to(device), tag_vecs.to(device)
+            unupdated_ids = np.setdiff1d(range(ntrain), data_ids)
+            ones = torch.ones(batch_size, 1).to(device)
+            ones_ = torch.ones(ntrain - batch_size, 1).to(device)
 
-
+            img_optimizer.zero_grad()
             img_out_batch = img_model(imgs)
 
             img_out[data_ids,:] = img_out_batch
             img_out_np = img_out.cpu().detach().numpy()
             img_out = torch.from_numpy(img_out_np).to(device)
 
-            theta = (1./2.)*torch.matmul(img_out, text_out.t())
+            theta = (1./2.)*torch.mm(img_out, text_out.t())
             theta_batch = theta[data_ids,:]
             hash_matrix_batch = hash_matrix[data_ids,:]
             sim_matrix_batch = sim_matrix[data_ids,:]
 
-            sim_sum = torch.sum(theta_batch*sim_matrix_batch - torch.log(1. + torch.exp(theta_batch)))
-            #preserve_sim = torch.sum(torch.pow(hash_matrix[data_idxs,:] - img_out, 2)) +\
-            #    torch.sum(torch.pow(hash_matrix[data_idxs,:] - text_out, 2))
-            #preserve_balance = torch.pow(img_out.matmul(ones), 2) +\
-            #    torch.pow(text_out.matmul(ones), 2)
+            sim_sum = -torch.sum(theta_batch*sim_matrix_batch - torch.log(1. + torch.exp(theta_batch)))
             preserve_sim = torch.sum(torch.pow(hash_matrix_batch - img_out_batch, 2))
-            preserve_balance = torch.sum(torch.pow(img_out.t().matmul(ones), 2))
-            loss = -sim_sum + gamma*preserve_sim + eta*preserve_balance
+            preserve_balance = torch.sum(torch.pow(img_out_batch.t().mm(ones) + img_out[unupdated_ids].t().mm(ones_), 2))
+            loss = sim_sum + gamma*preserve_sim + eta*preserve_balance
             loss /= (batch_size*ntrain)
 
-            img_optimizer.zero_grad()
             loss.backward()
+            print(img_out.grad)
             img_optimizer.step()
             loss_mean += loss.item()
         print('epoch: ', epoch, 'img_loss: ', loss_mean/len(train_loader))
@@ -184,24 +184,27 @@ def train(img_model, text_model, train_data, vocab_size, device):
         for batch_idx, (data_ids, data_idxs, imgs, tag_vecs) in enumerate(train_loader):
             batch_size = len(data_ids)
             imgs, tag_vecs = imgs.to(device), tag_vecs.to(device)
+            unupdated_ids = np.setdiff1d(range(ntrain), data_ids)
+            ones = torch.ones(batch_size, 1).to(device)
+            ones_ = torch.ones(ntrain - batch_size, 1).to(device)
 
-            text_out_batch = text_model(tag_vecs) 
+            text_optimizer.zero_grad()
+            text_out_batch = text_model(tag_vecs)
 
             text_out[data_ids,:] = text_out_batch
-            text_out_np = text_out.cpu().detach().numpy() 
+            text_out_np = text_out.cpu().detach().numpy()
             text_out = torch.from_numpy(text_out_np).to(device)
 
-            theta = (1./2.)*torch.matmul(img_out, text_out.t())
+            theta = (1./2.)*torch.mm(img_out, text_out.t())
             theta_batch = theta[data_ids,:]
             hash_matrix_batch = hash_matrix[data_ids,:]
             sim_matrix_batch = sim_matrix[data_ids,:]
 
-            sim_sum = torch.sum(theta_batch*sim_matrix_batch - torch.log(1. + torch.exp(theta_batch)))
+            sim_sum = -torch.sum(theta_batch*sim_matrix_batch - torch.log(1. + torch.exp(theta_batch)))
             preserve_sim = torch.sum(torch.pow(hash_matrix_batch - text_out_batch, 2))
-            preserve_balance = torch.sum(torch.pow(text_out.t().matmul(ones), 2))
-            loss = -sim_sum + gamma*preserve_sim + eta*preserve_balance
+            preserve_balance = torch.sum(torch.pow(text_out_batch.t().mm(ones) + text_out[unupdated_ids].t().mm(ones_), 2))
+            loss = sim_sum + gamma*preserve_sim + eta*preserve_balance
             loss /= (batch_size*ntrain)
-            text_optimizer.zero_grad()
             loss.backward()
             text_optimizer.step()
             loss_mean += loss.item()
@@ -243,7 +246,7 @@ def main():
     #print(vocab.itos)
     #print(vocab.stoi)
 
-    train_data = DcmhDataset('./train_saiapr.csv', vocab.stoi, vocab_size)
+    train_data = DcmhDataset('./train_saiapr_mini.csv', vocab.stoi, vocab_size)
 
     img_model = CNNModel(IMG_SIZE, HASH_CODR_LENGTH).to(device)
     text_model = TextModel(vocab_size, HASH_CODR_LENGTH).to(device)
